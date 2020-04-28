2f23ff6c8cbd4ac2c100c738c48a0fac76ff5f1d
aligator <aligator@suncraft-server.de>
2020-04-28 23:18:40 +0200

    refactor infill generation code

--- modify/infill.go
+++ modify/infill.go
@@ -30,59 +30,35 @@ func (m infillModifier) Modify(layerNr int, layers []data.PartitionedLayer) ([]d
 	}
 	// perimeters contains them as [part][insetNr][insetParts]
 
-	c := clip.NewClipper()
 	var bottomInfill []data.Paths
 	var topInfill []data.Paths
 
 	min, max := layers[layerNr].Bounds()
+	c := clip.NewClipper()
 	pattern := c.LinearPattern(min, max, m.options.Printer.ExtrusionWidth)
 
-	// TODO remove code duplication of top and bottom layer generation
 	// calculate the bottom parts for each inner perimeter part
 	for partNr, part := range perimeters {
 		// for the last (most inner) inset of each part
 		for insertPart, insetPart := range part[len(part)-1] {
 			fmt.Println("layerNr " + strconv.Itoa(layerNr) + " partNr " + strconv.Itoa(partNr) + " insertPart " + strconv.Itoa(insertPart))
-			if layerNr == 0 {
-				// for the first layer bottomInfill everything
-				bottomInfill = append(bottomInfill, c.Fill(insetPart, nil, m.options.Printer.ExtrusionWidth, pattern, m.options.Print.InfillOverlapPercent, internalInfillOverlap))
-				continue
-			} else if layerNr == len(layers)-1 {
-				// for the last layer topInfill everything
-				topInfill = append(topInfill, c.Fill(insetPart, nil, m.options.Printer.ExtrusionWidth, pattern, m.options.Print.InfillOverlapPercent, internalInfillOverlap))
-				continue
+
+			infill, err := m.genTopBottomInfill(insetPart, layerNr-1, layers, pattern)
+			if err != nil {
+				return nil, err
+			}
+			for _, paths := range infill {
+				bottomInfill = append(bottomInfill, paths)
+
 			}
 
-			// For the other layers detect the bottom parts by calculating the difference between the current most inner perimeter and the layer below.
-			// Also detect the top parts by calculating the difference between the current current most inner perimeter and the layer above
-			var toClipBelow []data.LayerPart
-			var toClipAbove []data.LayerPart
-
-			for _, belowPart := range layers[layerNr-1].LayerParts() {
-				toClipBelow = append(toClipBelow, belowPart)
+			infill, err = m.genTopBottomInfill(insetPart, layerNr+1, layers, pattern)
+			if err != nil {
+				return nil, err
 			}
+			for _, paths := range infill {
+				bottomInfill = append(bottomInfill, paths)
 
-			for _, abovePart := range layers[layerNr+1].LayerParts() {
-				toClipAbove = append(toClipAbove, abovePart)
-			}
-
-			fmt.Println("calculate difference with layer below")
-			toInfillBottom, ok := c.Difference(insetPart, toClipBelow)
-			if !ok {
-				return nil, errors.New("error while calculating difference with previous layer for detecting bottom parts")
-			}
-
-			fmt.Println("calculate difference with layer above")
-			toInfillTop, ok := c.Difference(insetPart, toClipAbove)
-			if !ok {
-				return nil, errors.New("error while calculating difference with next layer for detecting top parts")
-			}
-
-			for _, fill := range toInfillBottom {
-				bottomInfill = append(bottomInfill, c.Fill(fill, insetPart, m.options.Printer.ExtrusionWidth, pattern, m.options.Print.InfillOverlapPercent, internalInfillOverlap))
-			}
-			for _, fill := range toInfillTop {
-				topInfill = append(topInfill, c.Fill(fill, insetPart, m.options.Printer.ExtrusionWidth, pattern, m.options.Print.InfillOverlapPercent, internalInfillOverlap))
 			}
 		}
 	}
@@ -98,4 +74,36 @@ func (m infillModifier) Modify(layerNr int, layers []data.PartitionedLayer) ([]d
 	layers[layerNr] = newLayer
 
 	return layers, nil
+}
+
+// genInfill returns the infill for the top or bottom parts.
+// It calculates the difference of the layer with layerNr and the given part.
+// Then it fills the result by using the given pattern.
+func (m infillModifier) genTopBottomInfill(part data.LayerPart, layerNr int, layers []data.PartitionedLayer, pattern clip.Pattern) (result []data.Paths, err error) {
+	c := clip.NewClipper()
+
+	// for the first or last layer infill everything
+	if layerNr == -1 || layerNr == len(layers) {
+		result = append(result, c.Fill(part, nil, m.options.Printer.ExtrusionWidth, pattern, m.options.Print.InfillOverlapPercent, internalInfillOverlap))
+		return result, nil
+	}
+
+	// For the other layers detect the bottom parts by calculating the difference between the current most inner perimeter and the layer below.
+	// Also detect the top parts by calculating the difference between the current current most inner perimeter and the layer above
+	var toClip []data.LayerPart
+
+	for _, otherPart := range layers[layerNr].LayerParts() {
+		toClip = append(toClip, otherPart)
+	}
+
+	toInfill, ok := c.Difference(part, toClip)
+	if !ok {
+		return nil, errors.New("error while calculating difference for detecting bottom/top parts")
+	}
+
+	for _, fill := range toInfill {
+		result = append(result, c.Fill(fill, part, m.options.Printer.ExtrusionWidth, pattern, m.options.Print.InfillOverlapPercent, internalInfillOverlap))
+	}
+
+	return result, nil
 }
