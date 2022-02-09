94b139602a52a0df9a2e58be0f0c6c89a192f4e6
aligator <me@aligator.dev>
2022-02-09 22:43:00 +0100

    Start implementing the new algorithm

--- fast_slicer/slicer.go
+++ fast_slicer/slicer.go
@@ -2,11 +2,36 @@ package fast_slicer
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/aligator/goslice/clip"
 	"github.com/aligator/goslice/data"
 	"github.com/aligator/goslice/handler"
 )
+
+type Edge struct {
+	Point1 data.MicroVec3
+	Point2 data.MicroVec3
+}
+
+type IntersectionEdges struct {
+	ForwardEdge  *Edge
+	BackwardEdge *Edge
+}
+
+// Intersection is used to store the information related
+// to the intersection between the triangle and plane.
+// (Section 4.1.1)
+type Intersection struct {
+	Pre *Intersection
+
+	// IntersectionVertice between the forwardEdge and the current
+	// slicing Plane.
+	IntersectionVertice data.MicroVec3
+	Edge                *IntersectionEdges
+
+	Next *Intersection
+}
 
 type slicer struct {
 	options *data.Options
@@ -23,75 +48,48 @@ func (s slicer) Slice(m data.OptimizedModel) ([]data.PartitionedLayer, error) {
 	layers := make([]*layer, layerCount)
 
 	for i := 0; i < m.FaceCount(); i++ {
-		points := m.Face(i).Points()
-		minZ := points[0].Z()
-		maxZ := points[0].Z()
+		currentFace := m.Face(i)
+		currentOptimizedFace := m.OptimizedFace(i)
 
-		if points[1].Z() < minZ {
-			minZ = points[1].Z()
-		}
-		if points[2].Z() < minZ {
-			minZ = points[2].Z()
-		}
+		minZ := currentOptimizedFace.MinZ()
+		maxZ := currentOptimizedFace.MaxZ()
 
-		if points[1].Z() > maxZ {
-			maxZ = points[1].Z()
-		}
-		if points[2].Z() > maxZ {
-			maxZ = points[2].Z()
+		if minZ == maxZ {
+			continue
 		}
 
-		// for each layerNr
-		for layerNr := int((minZ - s.options.Print.InitialLayerThickness) / s.options.Print.LayerThickness); data.Micrometer(layerNr) <= (maxZ-s.options.Print.InitialLayerThickness)/s.options.Print.LayerThickness; layerNr++ {
-			z := data.Micrometer(layerNr)*s.options.Print.LayerThickness + s.options.Print.InitialLayerThickness
-			if z < minZ {
-				continue
-			}
-			if layerNr < 0 {
-				continue
+		points := currentFace.Points()
+
+		// vertices of the face sorted by min, med, max Z
+		vertices := points[:]
+		sort.Slice(vertices, func(i, j int) bool {
+			return vertices[i].Z() < vertices[j].Z()
+		})
+
+		slices := make([]int, 3)
+		for i, v := range vertices {
+			// Get the number of the slice where the vertice lies in.
+			// Take into account the thickness of the first layer and count +1 at the end for the ignored layer.
+			if v.Z() <= s.options.Print.InitialLayerThickness {
+				continue // leave the slice number = 0
 			}
 
-			if layers[layerNr] == nil {
-				layers[layerNr] = newLayer(layerNr, s.options)
-			}
+			// (Section 4.2)
+			// sj = (vj−szmin) / t
+			//
+			// where sj stands for s_min,s_med,s_max; vj stands for vzmin,
+			// vzmed,vzmax; szmin denotes the z coordinate of the 0th slice
+			// which can be designed to coincide to the x-y plane of the
+			// system; and t denotes the slice thickness.
 
-			layer := layers[layerNr]
-
-			var seg *segment
-			switch {
-			// only p0 is below z
-			case points[0].Z() < z && points[1].Z() >= z && points[2].Z() >= z:
-				seg = SliceFace(z, points[0], points[2], points[1])
-			// only p1 and p2 are below z
-			case points[0].Z() > z && points[1].Z() < z && points[2].Z() < z:
-				seg = SliceFace(z, points[0], points[1], points[2])
-
-			// only p1 is below z
-			case points[1].Z() < z && points[0].Z() >= z && points[2].Z() >= z:
-				seg = SliceFace(z, points[1], points[0], points[2])
-			// only p0 and p2 are below z
-			case points[1].Z() > z && points[0].Z() < z && points[2].Z() < z:
-				seg = SliceFace(z, points[1], points[2], points[0])
-
-			// only p2 is below z
-			case points[2].Z() < z && points[1].Z() >= z && points[0].Z() >= z:
-				seg = SliceFace(z, points[2], points[1], points[0])
-
-			// only p1 and p0 are below z
-			case points[2].Z() > z && points[1].Z() < z && points[0].Z() < z:
-				seg = SliceFace(z, points[2], points[0], points[1])
-			default:
-				// not all cases create a segment, because
-				// a point of a face could create just a dot
-				// and if all paths are below or above no face has to be created
-				continue
-			}
-
-			layer.faceToSegmentIndex[i] = len(layer.segments)
-			seg.faceIndex = i
-			seg.addedToPolygon = false
-			layer.segments = append(layer.segments, seg)
+			// Assume layer 0 to be on the bottom plane. -> can be ignored
+			slices[i] = int((v.Z()-s.options.Print.InitialLayerThickness)/s.options.Print.LayerThickness + 1)
 		}
+
+		// (Section 4.3)
+		// Judge forward and backwardEdge for {vecZMin, vecZMax} and {vecZMin, vecZMed}
+
+		// Judge forward and backwardEdge for {vecZMin, vecZMax} and {vecZMed, vecZMax}
 	}
 
 	retLayers := make([]data.PartitionedLayer, len(layers))
